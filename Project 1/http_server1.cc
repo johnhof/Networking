03acdadfcda2dd8df12fff087ba63b929c616a76
John Hofrichter <johnhofrichter@gmail.com>
2012-09-18 19:56:57 -0400

    reading complete, writing in progress
    no code was made, but everything is in place for revieving data, and
    thereturn header is almost written

--- Project 1/http_server1.cc
+++ Project 1/http_server1.cc
@@ -3,6 +3,10 @@
 #include <ctype.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <string>
+#include <fstream>
+#include <streambuf>
+using namespace std;
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
@@ -79,11 +83,11 @@ int main(int argc, char * argv[])
 /*--connection handling loop: wait to accept connection-------------------------*/
 
     while (1) {
-    int incomingSocket = -1;
-    struct sockaddr_in incomingAddr;
-    incomingSocket = minet_accept(serverSocket, &incomingAddr);
-    /* handle connections */
-    rc = handle_connection(incomingSocket);
+        int incomingSocket = -1;
+        struct sockaddr_in incomingAddr;
+        incomingSocket = minet_accept(serverSocket, &incomingAddr);
+        /* handle connections */
+        rc = handle_connection(incomingSocket);
     }
     
     minet_close(serverSocket);
@@ -97,10 +101,13 @@ int main(int argc, char * argv[])
 
 int handle_connection(int clientSocket) {
     bool ok = false;
-    char * buf;
-    memset(buf, 0, BUFSIZE);
-    String headerStr;
+    char * buffer;
+    memset(buffer, 0, BUFSIZE);
+    string headerStr;
+    string filePath;
+    string dataFromFile;
     int readSize;
+    int sizeToSend;
 
     char * ok_response_f = "HTTP/1.0 200 OK\r\n"    \
     "Content-type: text/plain\r\n"          \
@@ -115,21 +122,19 @@ int handle_connection(int clientSocket) {
 /*--first read loop -- get request and headers-----------------------------------*/
     while(true)
     {
-        readSize = minet_read(clientSocket, buf, BUFSIZE-1);//read the first chunk of data
+        readSize = minet_read(clientSocket, buffer, BUFSIZE-1);//read the first chunk of data
         
 
-        if(readSize + headerStr.size() <= 4)//if there are 4 or fewer char's in the header
+        if(readSize + headerStr.size() < 4 || readsize == 0)//check header integrity
         {
-            // if there are less than 4 char's, the header isn't properly formatted
-            if(readSize<4)fprintf(stderr, "Header not formatted properly\n", ); 
-            // if there are 4 char's, no file was specified
-            if(readSize==4)fprintf(stderr, "No file specified\n", );
+            if(readSize + headerStr.size()<4)fprintf(stderr, "Header not formatted properly\n"); 
+            if(readSize==0)fprintf(stderr, "Header not formatted properly\n"); 
             return -1;
         }
        
-        buf[readSize] = '\0';//add a NULL char so it can be read as a string
-        headerStr.append(buf);
-        if(buf.compare(strReq.size()-4,4,"\r\n\r\n"))
+        buffer[readSize] = '\0';//add a NULL char so it can be read as a string
+        headerStr.append(buffer);//append the buffer to the header string
+        if(buffer.compare(headerStr.size()-4,4,"\r\n\r\n")) break; //break when we read the end of the header
     }
 
 
@@ -137,13 +142,44 @@ int handle_connection(int clientSocket) {
 /*--parse request to get file ---------------------------------------------------*/
 /*Assumption: this is a GET request and filename contains no spaces*/
 
+    filePath.assign(headerStr.substr(4,headerStr.find(" ", 4));//cut off "GET " and "HTTP...\n"
 
 /*--try opening the file---------------------------------------------------------*/
+
+    //open the file
+    ifstream fileStream(filePath);
+    if(!fileStream.good())) 
+    {
+        ok = true;
+
+        //allocate the array memory before copying
+        fileStream.seekg(0, std::ios::end);   
+        str.reserve(fileStream.tellg());
+        fileStream.seekg(0, std::ios::beg);
+
+        //dump the file to a string
+        dataFromFile = string((std::istreambuf_iterator<char>(fileStream)),
+                                std::istreambuf_iterator<char>());
+    }
 
 /*--send response----------------------------------------------------------------*/
     if (ok) {
 /*--send headers-----------------------------------------------------------------*/
-    //minet_read();
+        char * headerStr;
+        //insert the length of the file into our headers file size indicator
+        sprintf(headerStr,ok_response_f,dataFromFile.size());
+
+        sizeToSend =  strlen(headerStr);
+        while(sizeToSend < n) {
+            sizesent = minet_write(clientSocket, headerStr, 1024);
+        
+            // if it didn't sent anything, error
+            if(num_sent < 0){
+                minet_perror("could not send request");
+                exit(4);
+            }
+            sizeToSend -= sizeSent;
+        }
 /*--send file--------------------------------------------------------------------*/
     //minet_write();
     } else {
@@ -154,6 +190,7 @@ int handle_connection(int clientSocket) {
   
     if (ok) {
         minet_close(clientSocket);
+        free(buffer);
     return 0;
     } else {
     return -1;
