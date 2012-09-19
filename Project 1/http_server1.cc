1e9fbe4df7de5c1ef7179587274701c041310402
John Hofrichter <johnhofrichter@gmail.com>
2012-09-18 20:08:56 -0400

    Make-able server, response header may work now

--- Project 1/http_server1.cc
+++ Project 1/http_server1.cc
@@ -125,7 +125,7 @@ int handle_connection(int clientSocket) {
         readSize = minet_read(clientSocket, buffer, BUFSIZE-1);//read the first chunk of data
         
 
-        if(readSize + headerStr.size() < 4 || readsize == 0)//check header integrity
+        if(readSize + headerStr.size() < 4 || readSize == 0)//check header integrity
         {
             if(readSize + headerStr.size()<4)fprintf(stderr, "Header not formatted properly\n"); 
             if(readSize==0)fprintf(stderr, "Header not formatted properly\n"); 
@@ -134,7 +134,7 @@ int handle_connection(int clientSocket) {
        
         buffer[readSize] = '\0';//add a NULL char so it can be read as a string
         headerStr.append(buffer);//append the buffer to the header string
-        if(buffer.compare(headerStr.size()-4,4,"\r\n\r\n")) break; //break when we read the end of the header
+        if(headerStr.compare(headerStr.size()-4,4,"\r\n\r\n")) break; //break when we read the end of the header
     }
 
 
@@ -142,19 +142,19 @@ int handle_connection(int clientSocket) {
 /*--parse request to get file ---------------------------------------------------*/
 /*Assumption: this is a GET request and filename contains no spaces*/
 
-    filePath.assign(headerStr.substr(4,headerStr.find(" ", 4));//cut off "GET " and "HTTP...\n"
+    filePath.assign(headerStr.substr(4,headerStr.find(" ", 4)));//cut off "GET " and "HTTP...\n"
 
 /*--try opening the file---------------------------------------------------------*/
 
     //open the file
     ifstream fileStream(filePath);
-    if(!fileStream.good())) 
+    if(!fileStream.good())
     {
         ok = true;
 
         //allocate the array memory before copying
         fileStream.seekg(0, std::ios::end);   
-        str.reserve(fileStream.tellg());
+        dataFromFile.reserve(fileStream.tellg());
         fileStream.seekg(0, std::ios::beg);
 
         //dump the file to a string
@@ -168,18 +168,32 @@ int handle_connection(int clientSocket) {
         char * headerStr;
         //insert the length of the file into our headers file size indicator
         sprintf(headerStr,ok_response_f,dataFromFile.size());
+        int sizeSent = 0;
 
+//////////can I just do this instead//////////////////////////////////////////////
+        sizeSent = minet_write(clientSocket, headerStr, strlen(headerStr));
+        // if it didn't sent anything, error
+        if(sizeSent < 0){
+            minet_perror("could not send header");
+            return -1;
+        }
+/*///////////////of this?/////////////////////////////////////////////////////////
+        //while we have data to send
         sizeToSend =  strlen(headerStr);
-        while(sizeToSend < n) {
-            sizesent = minet_write(clientSocket, headerStr, 1024);
+        while(sizeToSend > 0) {
+            //send it
+            sizeSent = minet_write(clientSocket, headerStr, strlen(headerStr));
         
             // if it didn't sent anything, error
-            if(num_sent < 0){
-                minet_perror("could not send request");
-                exit(4);
+            if(sizeSent < 0){
+                minet_perror("could not send header");
+                return -1;
             }
             sizeToSend -= sizeSent;
-        }
+        }*/
+
+
+
 /*--send file--------------------------------------------------------------------*/
     //minet_write();
     } else {
