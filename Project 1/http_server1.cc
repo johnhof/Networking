aaa1714c58bdc29a91f452663f521c83a53f39d9
John Hofrichter <johnhofrichter@gmail.com>
2012-09-22 13:53:09 -0400

    the main servers seem to work properly now

--- Project 1/http_server1.cc
+++ Project 1/http_server1.cc
@@ -5,234 +5,186 @@
 #include <sys/stat.h>
 #include <string>
 #include <fstream>
-#include <streambuf>
-
+#include <iostream>
 using namespace std;
 
-#define BUFSIZE 1024
+#define BUFFERSIZE 1024
 #define FILENAMESIZE 100
 
 int handle_connection(int sock);
+inline void sendMessage(int sock, char *buf, long n);
 
-int main(int argc, char * argv[]) 
-{
-    int serverPort = -1;
-    int rc          =  0;
-    char* serverName;
-    int serverSocket = -1;
-    struct sockaddr_in serverAddr;
-    int backlog = 5;
+int main(int argc, char * argv[]) {
+  int server_port = -1;
+  int rc          =  0;
+  int sock        = -1;
 
-/*--parse command line args-----------------------------------------------------*/
-    if (argc != 3) 
-    {
+  struct sockaddr_in addr;
+  const static int BACKLOG = 5;//number of connection can be waiting
+
+  /* parse command line args */
+  if (argc != 3) {
     fprintf(stderr, "usage: http_server1 k|u port\n");
     exit(-1);
+  }
+
+  server_port = atoi(argv[2]);
+
+  if (server_port < 1500) {
+    fprintf(stderr, "INVALID PORT NUMBER: %d; can't be < 1500\n", server_port);
+    exit(-1);
+  }
+   
+  /*init*/
+  if(toupper(argv[1][0]) == 'K') {
+    minet_init(MINET_KERNEL);
+  }
+  else if(toupper(argv[1][0]) == 'U') {
+    minet_init(MINET_USER);
+  }
+  else {
+    fprintf(stderr, "usage: http_server1 k|u port\n");
+    exit(-1);
+  }
+
+  /* initialize and make socket */
+  sock = minet_socket(SOCK_STREAM);
+  if(sock < 0)
+  {
+    minet_perror("Failed to create socket\n");
+    exit(1);
+  }
+
+  /* set server address*/
+  bzero(&addr, sizeof(addr));  
+  addr.sin_port = htons(server_port);
+  addr.sin_family = AF_INET;
+
+   
+  /* bind listening socket */
+  rc = minet_bind(sock, &addr);
+  if(rc < 0)
+  {
+    minet_perror("Failed to bind socket\n");
+    exit(2);
+  }
+
+  /* start listening */
+  rc = minet_listen(sock, BACKLOG);
+  if(rc < 0)
+  {
+    minet_perror("Failed to listen\n");
+    exit(3);
+  }
+
+  /* connection handling loop: wait to accept connection */
+  while (1) {
+    int accept_sock = minet_accept(sock,&addr);
+
+    if(accept_sock <= 0) {
+      fprintf(stderr, "Failed to accept");
+      continue;
     }
-
-    serverName = argv[0];
-    serverPort = atoi(argv[2]);
-
-
-    if (serverPort < 1500) 
-    {
-       fprintf(stderr, "INVALID PORT NUMBER: %d; can't be < 1500\n", serverPort);
-       exit(-1);
-    }
-
-/*--initialize and make socket---------------------------------------------------*/
-
-    if (toupper(*(argv[1])) == 'K') minet_init(MINET_KERNEL);
-    else if (toupper(*(argv[1])) == 'U') minet_init(MINET_USER);
-    else 
-    {
-        fprintf(stderr, "First argument must be k or u\n");
-        exit(-1);
-    }
-
-    serverSocket = minet_socket(SOCK_STREAM);//create the socket
-
-    //socket error
-    if(serverSocket < 0)
-    { 
-        perror("Failed to create socket");
-        exit(1);
-    }
-
-/*--set server address----------------------------------------------------------*/
-
-    serverAddr.sin_port = htons(serverPort);//Set the Port
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); //Support any IP
-    serverAddr.sin_family = AF_INET;//set to IP family
-
-/*--bind listening socket-------------------------------------------------------*/
-    
-    if(minet_bind(serverSocket,&serverAddr) < 0)
-    {
-        perror("Failed to bind socket");
-        exit(1);
-    }
-
-/*--start listening-------------------------------------------------------------*/
-
-    if(minet_listen(serverSocket, backlog)){
-        perror("Failed to listen");
-        exit(1);        
-    }
-
-/*--connection handling loop: wait to accept connection-------------------------*/
-
-    while (1) {
-        struct sockaddr_in incomingAddr;
-        int incomingSocket = minet_accept(serverSocket, &incomingAddr);
-
-        if(incomingSocket <= 0) {
-            fprintf(stderr, "No socket accepted");
-            perror("Could not accept socket connection");
-            continue;
-        }
-        /* handle connections */
-        rc = handle_connection(incomingSocket);
-    }
-    
-    minet_close(serverSocket);
-    minet_deinit();
-
+    /* handle connections */
+    rc = handle_connection(accept_sock);
+  }
+ 
+  minet_deinit();
 }
 
-/*-----------------------------------------------------------------------------*/
-/*HANDLE CONNECTION*/
-/*-----------------------------------------------------------------------------*/
+int handle_connection(int sock) {
+  bool ok = false;
+  string request_string;
+  string filename;
+  char buf[BUFFERSIZE];//create buffer
 
-int handle_connection(int clientSocket) {
-    bool ok = false;
-    char buffer [BUFSIZE];
-    string headerStr;
-    string filePath;
-    string dataFromFile;
-    int readSize;
-    int sizeToSend;
-
-
-    char * ok_response_f = "HTTP/1.0 200 OK\r\n"    \
-    "Content-type: text/plain\r\n"          \
+  static char * ok_response_f = "HTTP/1.0 200 OK\r\n"   \
+    "Content-type: text/plain\r\n"                      \
     "Content-length: %d \r\n\r\n";
- 
-    char * notok_response = "HTTP/1.0 404 FILE NOT FOUND\r\n"   \
-    "Content-type: text/html\r\n\r\n"           \
-    "<html><body bgColor=black text=white>\n"       \
+  static const int ok_response_len = strlen(ok_response_f);
+  static char * notok_response = "HTTP/1.0 404 FILE NOT FOUND\r\n"      \
+    "Content-type: text/html\r\n\r\n"                   \
+    "<html><body bgColor=black text=white>\n"           \
     "<h2>404 FILE NOT FOUND</h2>\n"
     "</body></html>\n";
-    
-/*--first read loop -- get request and headers-----------------------------------*/
-    while(true)
-    {
-        readSize = minet_read(clientSocket, buffer, BUFSIZE-1);//read the first chunk of data
-        if(readSize + headerStr.size() < 4 || readSize == 0)//check header integrity
-        {
-            if(readSize + headerStr.size()<4)fprintf(stderr, "Header not formatted properly\n"); 
-            if(readSize==0)fprintf(stderr, "Header not formatted properly\n"); 
-            return -1;
-        }
-        buffer[readSize] = '\0';//add a NULL char so it can be read as a string
-        headerStr.append(buffer);//append the buffer to the header string
-
-        //if we have read the header terminating string, break
-        if(headerStr.substr(headerStr.size()-4).compare("\r\n\r\n") == 0)break;
+  static const int notok_response_len = strlen(notok_response);
+   
+  /* first read loop -- get request and headers*/
+  int size = 0;
+  do {    
+    buf[size] = '\0';
+    request_string += buf;
+    if(request_string.size()>=4 && 0 == request_string.compare(request_string.size()-4,4,"\r\n\r\n")) {
+      break;
     }
+    size = minet_read(sock, buf,BUFFERSIZE);
+    if(size == 0) {
+      fprintf(stderr, "Connection Terminated\n");
+      return -1;
+    }
+    else if(size == -1) {//assuming this will never happen
+      fprintf(stderr, "Read Failure\n");
+      return -1;
+    }
+  }while(size > 0);
+   
+  /* parse request to get file name */
+  /* Assumption: this is a GET request and filename contains no spaces*/
 
-/*--parse request to get file ---------------------------------------------------*/
-/*Assumption: this is a GET request and filename contains no spaces*/
+  filename = request_string.substr(4,request_string.find(" ", 4)-4);
+  if(filename[0] == '\\' || filename[0] == '/') filename = filename.substr(1);
  
-    filePath.assign(headerStr.substr(4,headerStr.find(" ", 4)-4));//cut off "GET " and "HTTP...\n"
-
-
-/*--try opening the file---------------------------------------------------------*/
-
-    //open the file
-    ifstream fileStream(filePath);
-    if(fileStream.good())
-    {
-        ok = true;
-        //allocate the array memory before copying
-        fileStream.seekg(0, std::ios::end);   
-        dataFromFile.reserve(fileStream.tellg());
-        fileStream.seekg(0, std::ios::beg);
-
-
-        //dump the file to a string
-        dataFromFile = string((std::istreambuf_iterator<char>(fileStream)),
-                                std::istreambuf_iterator<char>());
+  /* try opening the file */
+  ifstream fin;// input file stream
+  fin.open(filename,ios::in);//open file
+  request_string.clear();
+  if(fin.is_open()) {
+    string tmp;
+    while(fin.good()) {
+      getline(fin, tmp);
+      request_string += tmp;
     }
+    ok = true;
+  }else {//fail to open file
+    ok =false;
+  }
+  fin.close();
 
-/*--send response----------------------------------------------------------------*/
-    if (ok) {
-/*--send headers-----------------------------------------------------------------*/
-        //char headerStr[sizeof(ok_response_f)+sizeof(dataFromFile.size())];
-        char headerStr[sizeof(ok_response_f)+dataFromFile.size()];
-        
-        //insert the length of the file into our headers file size indicator
-        sprintf(headerStr,ok_response_f,dataFromFile.size());
-        unsigned int sizeSent = 0;
+  /* send response */
+  if (ok) {
+    /* send headers */
+    char header[ok_response_len + 20];
+    sprintf(header, ok_response_f, request_string.size());
 
+    sendMessage(sock, header, strlen(header));
+    /* send file */
+    char *contentTmp = new char[request_string.size()+1];//create a tmp buffer
+    strcpy(contentTmp, request_string.data());
+    contentTmp[request_string.size()]= '\0';
+    sendMessage(sock, contentTmp,request_string.size());
+    delete contentTmp;
+   
+  } else {
+    sendMessage(sock, notok_response, notok_response_len);
+  }
+   
+  /* close socket and free space */
+  minet_close(sock);
+   
+  if(ok) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
 
-        //while we have data to send
-        unsigned int sizeToSend =  strlen(headerStr);
-        while(sizeSent<sizeToSend) {
-            //send it
-            sizeSent = minet_write(clientSocket, headerStr+sizeSent, strlen(headerStr));
-        
-            // if it didn't sent anything, error
-            if(sizeSent < 0){
-                minet_perror("could not send header");
-                return -1;
-            }
-            sizeSent++;
-        }
-
-
-
-/*--send file--------------------------------------------------------------------*/
-
-        sizeSent = 0;
-        //printf("about to send file: %i\n",dataFromFile.size());
-        //while we have data to send
-        while(sizeSent<dataFromFile.size()) {
-            string dataToSend = dataFromFile.substr(sizeSent);
-            //send it
-            sizeSent = minet_write(clientSocket, ((char *)(dataToSend.c_str()))+sizeSent, dataToSend.size());
-        
-            // if it didn't sent anything, error
-            if(sizeSent < 0){
-                minet_perror("No data sent");
-                return -1;
-            }
-            sizeSent++;
-        }
-    } else {//send error message
-        int sizeSent = 0;
-        sizeToSend =  strlen(notok_response);
-        while(sizeSent<sizeToSend) {
-            //send it
-            sizeSent = minet_write(clientSocket, notok_response+sizeSent, strlen(notok_response));
-        
-            // if it didn't sent anything, error
-            if(sizeSent < 0){
-                minet_perror("Could not send error");
-                return -1;
-            }
-            sizeSent++;
-        }
+/*inline function for sending msg over socket*/
+inline void sendMessage(int sock, char *buf, long n) {
+  long total_bytes_sent = 0;
+  while(total_bytes_sent < n) {
+    int size = minet_write(sock, buf+total_bytes_sent, n-total_bytes_sent);
+    if(size > 0) {
+      total_bytes_sent += size;
     }
-    
- /*--close socket and free space-------------------------------------------------*/
-  
-    minet_close(clientSocket);
-    minet_deinit();
-    
-    if (ok) {
-        return 0;
-    } else {
-        return -1;
-    }
+  }
 }
