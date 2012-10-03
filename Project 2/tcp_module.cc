bb56cb4db9c79d8d3e5a6ee7bb92530f65bc3ad9
John Hofrichter <johnhofrichter@gmail.com>
2012-10-03 06:33:03 -0400

    handshake code is written. but not compiling

--- Project 2/tcp_module.cc
+++ Project 2/tcp_module.cc
@@ -52,22 +52,43 @@ struct TCPState {
 //--------------------------------------------------------------------------------------------------
 //----HELPER FUNCITON HEADERS
 //--------------------------------------------------------------------------------------------------
-int handshake();
+void handShake(Packet p);
 void handlePacket(Packet p);
 
 
 //--------------------------------------------------------------------------------------------------
 //----GLOBAL VARIABLES
 //--------------------------------------------------------------------------------------------------
+IPHeader ipHead;
 int connState;
 int targetPort;
 char * targetIP;
 int myPort;
+unsigned int seqNum;
+unsigned int ackNum;
 
 //--------------------------------------------------------------------------------------------------
 //----MAIN
 //--------------------------------------------------------------------------------------------------
 int main(int argc, char * argv[]) {
+
+//---- initialize of global variables --------------------------------------------------------------
+
+//!!!! NOTE: not sure what Ip to use here------
+	targetIP = "192.168.102.100";
+//!!!! NOTE: or here---------------------------
+	targetPort = 5000;
+	myPort = 500;
+	connState = 0;//start with the state set to closed
+	seqNum = 1;
+
+//!!!! NOTE: unsure on this bit, its legit as far as I can tell
+	ipHead.SetProtocol(IP_PROTO_TCP);
+	ipHead.SetSourceIP(MyIPAddr());
+	ipHead.SetDestIP(IPAddress(targetIP));
+	ipHead.SetTotalLength(TCP_HEADER_BASE_LENGTH+IP_HEADER_BASE_LENGTH);
+
+
     MinetHandle mux;
     MinetHandle sock;
     
@@ -106,22 +127,52 @@ int main(int argc, char * argv[]) {
     MinetEvent event;
 
 //-- execute initial handshake ------------------------------------------------------------------------
-    //handShake();
+    
+    handShake();
 
     double timeout = 1;
 
-    while (MinetGetNextEvent(event, timeout) == 0) {
+    while (MinetGetNextEvent(event, timeout) == 0) 
+    {
 
 		if ((event.eventtype == MinetEvent::Dataflow) && 
 		    (event.direction == MinetEvent::IN)) 
 		{
-		
+//-- PRESUMABLY WE WILL FALL INTO THIS IF DUING AN NC CALL --------------------------------------------
 		    if (event.handle == mux) 
 		    {
 				// ip packet has arrived!
 				Packet p;
 				MinetReceive(mux, p);
-				handlePacket(p);
+				switch(connState)
+				{
+					case LISTEN:
+						//Should never reach this state.
+						break;
+					case SYN_RCVD:
+					case SYN_SENT:
+						handshake(P)
+						break;
+					case ESTABLISHED:
+						//operate normally
+						break;
+					case FIN_WAIT1:
+						//recieve ACK, go to FIN_WAIT2
+						break;
+					case FIN_WAIT2:
+						//recieve FIN, send ACK, go to TIME_WAIT
+						break;
+					case TIME_WAIT:
+						//Wait?
+						break;
+					case CLOSE_WAIT:
+						//Send FIN and ACK after receiving FIN
+						break;
+					case LAST_ACK:
+						//terminate connection
+						break;
+				}
+//-- THIS SHOULD PRINT THE PACKET CONTENTS -----------------------------------------------------------
 				cout << p << endl;
 		    }
 
@@ -143,7 +194,6 @@ int main(int argc, char * argv[]) {
 						response.bytes=0;
 						response.error=EOK;
 						
-//----------------------handshake?-----------------------------------------------------------
 
 						MinetSend(sock,response);
 					}
@@ -223,7 +273,8 @@ int main(int argc, char * argv[]) {
 		    }
 		}
 
-		if (event.eventtype == MinetEvent::Timeout) {
+		if (event.eventtype == MinetEvent::Timeout) 
+		{
 		    // timeout ! probably need to resend some packets
 		}
 
@@ -244,48 +295,126 @@ int main(int argc, char * argv[]) {
 //--------------------------------------------------------------------------------------------------
 //----handShake()
 //		performs three part handshake to establish a connection
+//		NOTE: must be called several times, it acts according to state
 //--------------------------------------------------------------------------------------------------
 
-int handShake()
+void handShake(Packet inPacket)
 {
-	return 0;
-}
-
-
-//--------------------------------------------------------------------------------------------------
-//----receive()
-//		takes a packet and performs the appropriate operation based on the state
-//			-Packet p: incoming packet
-//--------------------------------------------------------------------------------------------------
-
-void handlePacket(Packet p){
-	switch(connState){
-		case LISTEN:
-			//Should never reach this state.
-			break;
+	switch(connState)
+	{
+		//We have recieved a SYN to the initial shake,
+		//deal with it and  send our response
 		case SYN_RCVD:
-			//handshake part1
+		{
+			Packet outPacket;
+			unsigned short destPort=0;
+
+			//grab the header and perform the checksum
+			inPacket.ExtractHeaderFromPayload<TCPHeader>(
+				TCPHeader::EstimateTCPHeaderLength(inPacket));			
+			TCPHeader inHeader;
+			inHeader = inPacket.FindHeader(Headers::TCPHeader);
+			bool checksum = inHeader.IsCorrectChecksum(inPacket);
+			if(!checksum){
+				return;
+			}
+
+			//retrieve the seqnum and the port
+			inHeader.GetSeqNum(ackNum);
+			outHeader.SetAckNum(ackNum++, outPacket);//dont forget to increment the ACK!
+			outPacket.PushFrontHeader(ipHead);
+			TCPHeader outHeader;
+			inHeader.GetSourcePort(destPort);
+			outHeader.SetDestPort(destPort, outPacket);
+
+
+			outHeader.SetSourcePort(myPort, outPacket);
+			outHeader.SetHeaderLen(TCP_HEADER_BASE_LENGTH, outPacket);
+//!!!! NOTE: I assume we need a better way to choose the starting seqnum 
+			//But hardcoding to 50 should do for now
+			outHeader.SetSeqNum(50, outPacket);
+
+//!!!! NOTE (OC) this is set to zero
+			SET_ACK(0);
+			SET_SYN(0);
+			outHeader.SetFlags(0, outPacket);
+//!!!! NOTE: The window size is hardcoded to 100, should it be dynamic?
+			outHeader.SetWinSize(100, outPacket);
+			outPacket.PushBackHeader(outHeader);
+			MinetSend(mux, outPacket);
+			//sleep(2);
+			//MinetSend(mux,outPacket);
+			connState = ESTABLISHED;
+		}
 			break;
+		//Our SYN has been sent, finish up and handle the packet
 		case SYN_SENT:
-			//finish handshale
+		{
+			Packet outPacket;
+			//last_acked = GetAckNum(inPacket);
+
+			//retrieve header and perform chescksum
+			inPacket.ExtractHeaderFromPayload<TCPHeader>(TCPHeader::EstimateTCPHeaderLength(inPacket));
+			TCPHeader inHeader;
+			inHeader = inPacket.FindHeader(Headers::TCPHeader);
+			bool checksum = inHeader.IsCorrectChecksum(inPacket);
+			if(!checksum)return;
+
+			//get the seq and acks fron the header
+			inHeader.GetSeqNum(seqNum);
+			inHeader.GetAckNum(ackNum);
+
+			//set up the header
+			outPacket.PushFrontHeader(ipHead);
+			TCPHeader outHeader;
+			outHeader.SetSourcePort(myPort, outPacket);
+			outHeader.SetDestPort(targetPort, outPacket);
+			outHeader.SetHeaderLen(TCP_HEADER_BASE_LENGTH,outPacket);
+			SET_ACK(0);
+			outHeader.SetFlags(0, outPacket);
+			outHeader.SetWinSize(TCP_MAXIMUM_SEGMENT_SIZE,outPacket);
+			//dont forget to increment the seq and ack!
+			outHeader.SetAckNum(ackNum++,outPacket);
+			outHeader.SetSeqNum(seqNum++,outPacket);
+
+			//tack the header to the packet abd send it
+			outPacket.PushBackHeader(outHeader);
+			MinetSend(mux, outPacket);
+			connState = ESTABLISHED;
+
+//!!!! NOTE: not sure on this
+			packetMailer(false,false,false,l,sizeof(l));
+		}
 			break;
-		case ESTABLISHED:
-			//operate normally
-			break;
-		case FIN_WAIT1:
-			//recieve ACK, go to FIN_WAIT2
-			break;
-		case FIN_WAIT2:
-			//recieve FIN, send ACK, go to TIME_WAIT
-			break;
-		case TIME_WAIT:
-			//Wait?
-			break;
-		case CLOSE_WAIT:
-			//Send FIN and ACK after receiving FIN
-			break;
-		case LAST_ACK:
-			//terminate connection
-			break;
+		//if we are not in the SYN_RCVD or SYN_SENT states, 
+		//intitate the shake by sending our SYN
+		default:
+		{
+			//tack on the IP header for the destination
+			Packet p;
+			p.PushFrontHeader(ipHead);
+
+			//now set the TCP header ports and length
+			TCPHeader header;
+			header.SetSourcePort(myPort, p);
+			header.SetDestPort(targetPort, p);
+			header.SetHeaderLen(TCP_HEADER_BASE_LENGTH, p);//sets len and recomputes checksum
+			
+			//set up the ACK & SYN
+			unsigned char ack = 0;
+			seqNum = 1;
+			header.SetSeqNum(seqNum, p);
+//!!!! NOTE: cant find any documentation on this (OC: SET_SYN is always 0?)
+			SET_SYN(ack);
+			header.SetFlags(ack, p);//sets flags and recalculates checksum
+
+			//tack the TCP header onto the packet and send it
+			p.PushBackHeader(header);
+			MinetSend(mux,p);
+			//--OC
+			//sleep(2);
+			//MinetSend(mux,p);
+			connState = SYN_SENT;
+		}
 	}
 }
